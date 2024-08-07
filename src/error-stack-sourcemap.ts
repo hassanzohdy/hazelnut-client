9244ffe608c5c75283494ad178e1ebe14fe45bf8
hassanzohdy <hassanzohdy@gmail.com>
2024-08-07 16:30:15 +0300

    "Ignored parsing sourcemap errors"

--- src/error-stack-sourcemap.ts
+++ src/error-stack-sourcemap.ts
@@ -5,7 +5,7 @@ const fetchingUrls: string[] = [];
 
 export async function parseStackTraceFromSourcemap(
   stack: StackFrame[],
-  sourcemapUrlParser: (file: string) => string = file => file + ".map",
+  sourcemapUrlParser: (file: string) => string = (file) => file + ".map"
 ): Promise<StackFrame[]> {
   // first off get all file names urls to be fetched
   for (const callSite of stack) {
@@ -24,7 +24,7 @@ export async function parseStackTraceFromSourcemap(
   if (fetchingUrls.length > 0) {
     try {
       await loadSourcemapFiles(
-        fetchingUrls.map(file => sourcemapUrlParser(file)),
+        fetchingUrls.map((file) => sourcemapUrlParser(file))
       );
 
       // clear the fetching urls
@@ -38,22 +38,26 @@ export async function parseStackTraceFromSourcemap(
 
   // source maps urls are teh same as js file name suffixed with .map
   for (const callSite of stack) {
-    // source map object type is the first parameter of SourceMapConsumer
-    const sourceMap = sourcemapUrls[sourcemapUrlParser(callSite.fileName!)];
+    try {
+      // source map object type is the first parameter of SourceMapConsumer
+      const sourceMap = sourcemapUrls[sourcemapUrlParser(callSite.fileName!)];
 
-    const consumer = await new SourceMapConsumer(sourceMap);
+      const consumer = await new SourceMapConsumer(sourceMap);
 
-    const originalPosition = consumer.originalPositionFor({
-      line: callSite.lineNumber!,
-      column: callSite.columnNumber!,
-    });
+      const originalPosition = consumer.originalPositionFor({
+        line: callSite.lineNumber!,
+        column: callSite.columnNumber!,
+      });
 
-    // now replace the callSite with the original position
-    callSite.fileName = originalPosition.source;
-    callSite.lineNumber = originalPosition.line;
-    callSite.columnNumber = originalPosition.column;
-    if (originalPosition.name) {
-      callSite.functionName = originalPosition.name;
+      // now replace the callSite with the original position
+      callSite.fileName = originalPosition.source;
+      callSite.lineNumber = originalPosition.line;
+      callSite.columnNumber = originalPosition.column;
+      if (originalPosition.name) {
+        callSite.functionName = originalPosition.name;
+      }
+    } catch {
+      //
     }
   }
 
@@ -62,13 +66,13 @@ export async function parseStackTraceFromSourcemap(
 
 export async function loadSourcemapFiles(fileNames: string[]) {
   return await Promise.all(
-    fileNames.map(async fileName => {
+    fileNames.map(async (fileName) => {
       if (sourcemapUrls[fileName]) return sourcemapUrls[fileName];
 
       const response = await fetch(fileName);
       const text = await response.json();
       sourcemapUrls[fileName] = text;
       return text;
-    }),
+    })
   );
 }
